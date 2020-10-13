cb1329cb5f4d64aa3f084493308d5eae43880940
AleksaC <aleksacukovic1@gmail.com>
2020-10-13 20:54:45 +0200

    Add initial working version of bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,33 +1,113 @@
 #!/usr/bin/env python
+
 import os
+import re
 import sys
 
+from typing import Dict
+from typing import Set
+from typing import Union
 
-PROJECT_REMOTE = "git@github.com:AleksaC/drf-base.git __project__"
+
+VARIABLE_RE = re.compile("___(?P<var_name>[a-zA-Z_][a-zA-Z_0-9]*)___")
+
+PROJECT_REMOTE = "git@github.com:AleksaC/test-template.git"
+
+IGNORE = {
+    ".git",
+    ".idea",
+    ".vscode",
+    "venv",
+    "_build",
+    "dist",
+    "build",
+    "__pycache__",
+    "library.egg-info",
+}
+
+variables = {"repo_name"}
+file_paths: Set[str] = set()
+file_contents = {}
 
 
-def bootstrap():
+def traverse_repo(root: Union[str, os.DirEntry]) -> None:
+    with os.scandir(root) as it:
+        for entry in it:
+            name = entry.name
+            if name not in IGNORE:
+                variable = re.match(VARIABLE_RE, name)
+                if variable:
+                    var_name = variable.group("var_name")
+                    variables.add(var_name)
+                    path = entry.path
+                    for fp in file_paths.copy():
+                        if path.startswith(fp):
+                            file_paths.remove(fp)
+                            break
+                    file_paths.add(path)
+                if entry.is_dir():
+                    traverse_repo(entry)
+                else:
+                    with open(entry.path) as f:
+                        try:
+                            contents = f.read()
+                            var = set(re.findall(VARIABLE_RE, contents))
+                            if var:
+                                variables.update(var)
+                                file_contents[entry.path] = var
+                        except UnicodeDecodeError:
+                            pass
+
+
+def inquire(variables: Set[str]) -> Dict[str, str]:
+    values = {}
+    print("Enter the values for the following variables:")
+    for variable in variables:
+        values[variable] = input(f"{variable}: ")
+    return values
+
+
+def render_files(fc: Dict[str, Set[str]], values: Dict[str, str]) -> None:
+    for fp, vrs in fc.items():
+        with open(fp, "r+") as f:
+            c = f.read()
+            for var in vrs:
+                c = c.replace(f"___{var}___", values[var])
+            f.seek(0)
+            f.write(c)
+            f.truncate()
+
+
+def render_paths(fps: Set[str], values: Dict[str, str]) -> None:
+    for fp in fps:
+        path = ""
+        for name in re.split(VARIABLE_RE, fp):
+            if name in values:
+                extended_path = path + values[name]
+                os.rename(f"{path}___{name}___", extended_path)
+                path = extended_path
+            else:
+                path += name
+
+
+def bootstrap() -> None:
     if __file__ == "<stdin>":
-        os.system("git clone %s" % PROJECT_REMOTE)
-        os.execl(sys.executable, sys.executable, "__project__/bootstrap.py")
+        os.system("git clone %s ___repo_name___" % PROJECT_REMOTE)
+        os.execl(sys.executable, sys.executable, "___repo_name___/bootstrap.py")
 
     if not sys.stdin.isatty():
         sys.stdin = open("/dev/tty")
 
-    project_name = input("Project_name: ")
+    traverse_repo("___repo_name___")
+    values = inquire(variables)
+    render_files(file_contents, values)
+    render_paths(file_paths, values)
 
-    path = os.path.realpath(__file__)
-    project_base = os.path.dirname(path)
-    project_base_location = os.path.dirname(project_base)
-    new_project_base = os.path.join(project_base_location, project_name)
-
-    os.rename(project_base, new_project_base)
-
-    current_file_name = os.path.basename(path)
-    os.remove(os.path.join(new_project_base, current_file_name))
+    os.remove(os.path.realpath(__file__))
 
 
-def main():
+def main() -> int:
+    bootstrap()
     return 0
 
 
--- test_bootstrap.py
+++ test_bootstrap.py
@@ -1,5 +1,22 @@
-from bootstrap import main
+import bootstrap
+
+
+def test_render_path(tmpdir):
+    path = tmpdir / "___test___"
+    path.mkdir()
+    path = path / "___dir___"
+    path.mkdir()
+
+    values = {"test": "subdir1", "dir": "subdir2"}
+    bootstrap.render_paths({str(path)}, values)
+
+    subdir = tmpdir.listdir()[0]
+    print(subdir)
+    assert str(subdir) == str(tmpdir / "subdir1")
+
+    subdir = subdir.listdir()[0]
+    assert str(subdir) == str(tmpdir / "subdir1" / "subdir2")
 
 
 def test_main():
-    assert main() == 0
+    assert bootstrap.main() == 0

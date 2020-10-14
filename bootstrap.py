8766954572cabdd21b9da6955e871c3eb29c7688
AleksaC <aleksacukovic1@gmail.com>
2020-10-15 01:47:06 +0200

    Add initial version that works end to end

--- bootstrap.py
+++ bootstrap.py
@@ -2,7 +2,11 @@
 
 import os
 import re
+import shutil
 import sys
+from urllib.error import HTTPError
+from urllib.request import Request
+from urllib.request import urlopen
 
 from typing import Dict
 from typing import Set
@@ -13,6 +17,8 @@ VARIABLE_RE = re.compile("___(?P<var_name>[a-zA-Z_][a-zA-Z_0-9]*)___")
 
 PROJECT_REMOTE = "git@github.com:AleksaC/test-template.git"
 
+# TODO: Look for a better way of excluding files than hard-coding them here.
+#  Maybe try parsing .gitignore (.git should stay hardcoded anyway)
 IGNORE = {
     ".git",
     ".idea",
@@ -25,7 +31,7 @@ IGNORE = {
     "library.egg-info",
 }
 
-variables = {"repo_name"}
+variables: Set[str] = set()
 file_paths: Set[str] = set()
 file_contents = {}
 
@@ -90,24 +96,105 @@ def render_paths(fps: Set[str], values: Dict[str, str]) -> None:
                 path += name
 
 
-def bootstrap() -> None:
+def create_github_repo(repo_name: str) -> str:
+    user_or_org = ""
+    while user_or_org not in {"u", "o"}:
+        user_or_org = input(
+            "Are you creating a repository for a user or an organization? [u/o] "
+        )
+    print()
+    user = user_or_org == "u"
+    user_or_org_name = input(f"{'User' if user else 'Organization'} name: ")
+    token = input("Enter github personal access token: ")
+
+    url = "https://api.github.com/"
+    if user:
+        url += "user/repos"
+    else:
+        url += f"orgs/{user_or_org_name}/repos"
+    headers = {"Authorization": f"token {token}"}
+    data = f'{{"name":"{repo_name}"}}'
+
+    req = Request(url, headers=headers, data=data.encode())
+    print("\nCreating GitHub repo...\n")
+    try:
+        resp = urlopen(req)
+    except HTTPError as e:
+        print(
+            "Something went wrong - please check that the repository with the name "
+            f"{repo_name} doesn't already exist. Also make sure that the personal "
+            "access token you provided is correct."
+        )
+        print(e)
+        return ""
+
+    if resp.getcode() == 201:
+        return f"{user_or_org_name}/{repo_name}"
+
+    return ""
+
+
+def push_to_github(repo_path: str) -> int:
+    url = f"git@github.com:{repo_path}"
+
+    commands = [
+        "git add .",
+        "git commit -m 'Initial commit'",
+        f"git remote add origin {url}",
+        "git push -u origin HEAD",
+    ]
+
+    for command in commands:
+        status = os.system(command)
+        if status != 0:
+            return status
+
+    return 0
+
+
+def main() -> int:
     if __file__ == "<stdin>":
-        os.system("git clone %s ___repo_name___" % PROJECT_REMOTE)
-        os.execl(sys.executable, sys.executable, "___repo_name___/bootstrap.py")
+        os.system(f"git clone --depth 1 {PROJECT_REMOTE} ___repo_name___")
+        os.execl(
+            sys.executable,
+            sys.executable,
+            os.path.join("___repo_name___", "bootstrap.py"),
+        )
 
     if not sys.stdin.isatty():
         sys.stdin = open("/dev/tty")
 
-    traverse_repo("___repo_name___")
+    repo_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(repo_path)
+
+    if os.path.exists(".git"):
+        shutil.rmtree(".git")
+
+    traverse_repo(os.getcwd())
     values = inquire(variables)
     render_files(file_contents, values)
     render_paths(file_paths, values)
 
-    os.remove(os.path.realpath(__file__))
+    print()
+    os.system("git init")
+    print()
 
+    os.remove(os.path.join(os.getcwd(), os.path.basename(__file__)))
 
-def main() -> int:
-    bootstrap()
+    publish_to_github = ""
+    while publish_to_github not in {"y", "n"}:
+        publish_to_github = input(
+            "Publish the repo to github? (Requires personal access token) [y/n] "
+        )
+
+    if publish_to_github == "y":
+        print()
+        repo_path = create_github_repo(values["repo_name"])
+        if repo_path:
+            push_to_github(repo_path)
+        else:
+            print("Could not create the repository!")
+
     return 0
 
 
--- test_bootstrap.py
+++ test_bootstrap.py
@@ -1,22 +1,105 @@
+import io
+
+import pytest
+
 import bootstrap
 
 
-def test_render_path(tmpdir):
+@pytest.fixture
+def path(tmpdir):
     path = tmpdir / "___test___"
     path.mkdir()
     path = path / "___dir___"
     path.mkdir()
+    return path
 
+
+@pytest.fixture
+def file(tmp_path):
+    path = tmp_path / "test_file.txt"
+    file_contents = (
+        "Lorem ipsum ___var1___ sit amet, consectetur adipiscing elit, sed do eiusmod "
+        "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, "
+        "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo "
+        "consequat. Duis aute irure ___var1___ in reprehenderit in voluptate velit "
+        "esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat "
+        "cupidatat non proident, sunt in ___var2___ qui officia deserunt mollit anim "
+        "id est laborum."
+    )
+    path.write_text(file_contents)
+    return path
+
+
+def test_traverse_repo(path, file):
+    root = file.parent
+    (root / ".git").mkdir()
+    subdir = root / "___subdir___"
+    subdir.mkdir()
+    (root / "no_match.txt").write_text("No match here")
+
+    bootstrap.traverse_repo(str(root))
+
+    assert {"dir", "test", "subdir", "var1", "var2"} == bootstrap.variables
+    assert {str(path), str(subdir)} == bootstrap.file_paths
+    assert {str(file): {"var1", "var2"}} == bootstrap.file_contents
+
+
+def test_traverse_repo_binary_file():
+    pass
+
+
+def test_inquire(capsys, monkeypatch):
+    variables = {"var1", "var2"}
+    values = io.StringIO("val1\nval2\n")
+    monkeypatch.setattr("sys.stdin", values)
+
+    res = bootstrap.inquire(variables)
+
+    out, _ = capsys.readouterr()
+    if "var1: var2: " in out:
+        expected = {
+            "var1": "val1",
+            "var2": "val2",
+        }
+    else:
+        expected = {
+            "var2": "val1",
+            "var1": "val2",
+        }
+
+    assert res == expected
+
+
+def test_render_files(file):
+    values = {"var1": "dolor", "var2": "culpa"}
+
+    bootstrap.render_files({str(file): set(values)}, values)
+
+    assert file.read_text() == (
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod "
+        "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, "
+        "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo "
+        "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse "
+        "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non "
+        "proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+
+
+def test_render_paths(path, tmpdir):
     values = {"test": "subdir1", "dir": "subdir2"}
+
     bootstrap.render_paths({str(path)}, values)
 
     subdir = tmpdir.listdir()[0]
-    print(subdir)
     assert str(subdir) == str(tmpdir / "subdir1")
 
     subdir = subdir.listdir()[0]
     assert str(subdir) == str(tmpdir / "subdir1" / "subdir2")
 
 
-def test_main():
-    assert bootstrap.main() == 0
+def test_create_github_repo():
+    pass
+
+
+def test_push_to_github():
+    pass
